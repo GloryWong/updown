f3a096e1e9232d7cd2093a8ad228b8a2bbed1795
Glory Wong <glorywong1001@gmail.com>
2025-03-19 00:26:46 +0800

    fix: add config for tabby config

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,8 @@
 import { Configs } from '../types/configs.d.ts'
 import brewfile from './brewfile.ts'
+import tabbyConfig from './tabby-config.ts'
 import vimrc from './vimrc.ts'
 import zshrc from './zshrc.ts'
 
-const configs: Configs = [brewfile, vimrc, zshrc]
+const configs: Configs = [brewfile, vimrc, zshrc, tabbyConfig]
 export default configs
---
+++ configs/tabby-config.ts
@@ -0,0 +1,24 @@
+import { os, path } from 'zx'
+import { Config } from '../types/configs.d.ts'
+
+export default {
+  name: 'tabby-config.yaml',
+  beforeUpload: () => {
+    const home = os.homedir()
+    const configFile = 'config.yaml'
+    const fileLocations = [
+      {
+        name: 'darwin',
+        value: path.join(home, 'Library/Application Support/tabby', configFile),
+      },
+      { name: 'win32', value: path.join(home, 'AppData/Tabby', configFile) },
+      { name: 'linux', value: path.join(home, '.config/tabby', configFile) },
+    ]
+
+    const platform = os.platform()
+    const fileLocation = fileLocations.find(({ name }) => platform === name)
+    if (!fileLocation) throw new Error(`Unsupported platform ${platform}`)
+
+    return fileLocation.value
+  },
+} satisfies Config
